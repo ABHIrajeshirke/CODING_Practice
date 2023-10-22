dab410930a03a15c3df6563538db8d667c8ae6f0
Abhishek Rajeshirke <146904201+ABHIrajeshirke@users.noreply.github.com>
2023-10-23 01:12:04 +0530

    Create EX01_Disp_1to10.java

---
+++ Day_1/EX01_Disp_1to10.java
@@ -0,0 +1,13 @@
+package Day_1;
+
+public class EX01_Disp_1to10 
+{
+	public static void main(String[] args) 
+		{
+			System.out.println("Numbers from 1 to 10: ");
+			for(int i=1; i<=10; i++) 
+			{
+				System.out.print(" "+i);
+			}
+		}
+}
