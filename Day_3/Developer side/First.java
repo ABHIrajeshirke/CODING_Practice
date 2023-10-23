e8142342a2be0691a24378097801b83dca492fc8
Abhishek Rajeshirke <146904201+ABHIrajeshirke@users.noreply.github.com>
2023-10-23 19:30:15 +0530

    Create First.java

---
+++ Day_3/Developer side/First.java
@@ -0,0 +1,24 @@
+package Developer;
+
+public class First 
+{
+	private int num;
+	
+	public First(int x) 
+	{
+		this.num = x;
+	}
+	
+	public int getNum() 
+	{
+		return num;
+	}
+	
+	public void setNum(int num) 
+	{
+		this.num = num;
+	}
+}
+	
+
+	
